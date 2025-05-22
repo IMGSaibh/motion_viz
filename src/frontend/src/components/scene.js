68bfbaef1e3667458ce9a873c3ce610c66c47d7e
MatGew99 <mathias.gewissen@htw-dresden.de>
2025-05-22 10:22:49 +0200

    added localhost variation to CORSMiddleware

--- src/frontend/src/components/scene.js
+++ src/frontend/src/components/scene.js
@@ -8,9 +8,9 @@ function createScene() {
   const gridHelper = new GridHelper(3000, 60);
   scene.add(gridHelper, light);
 
-  const xAxis = createThickAxis(0xff0000, [0,0,0], [25,0,0], 5);
-  const yAxis = createThickAxis(0x00ff00, [0,0,0], [0,25,0], 5);
-  const zAxis = createThickAxis(0x0000ff, [0,0,0], [0,0,25], 5);
+  const xAxis = createThickAxis(0xff0000, [0,0,0], [50,0,0], 3);
+  const yAxis = createThickAxis(0x00ff00, [0,0,0], [0,50,0], 3);
+  const zAxis = createThickAxis(0x0000ff, [0,0,0], [0,0,50], 3);
   scene.add(xAxis, yAxis, zAxis);  
   
   scene.background = new Color('black');
--- src/motion_viz/main.py
+++ src/motion_viz/main.py
@@ -11,7 +11,7 @@ favicon_path = 'src/frontend/public/human.ico'
 # allow CORS for React-Vite
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
