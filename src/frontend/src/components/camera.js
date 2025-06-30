aec8a1bd38f7eba2452503bd8af87d73b21d3fde
MatGew99 <mathias.gewissen@htw-dresden.de>
2025-06-30 11:15:30 +0200

    camera adjustment

--- src/frontend/src/components/camera.js
+++ src/frontend/src/components/camera.js
@@ -9,14 +9,7 @@ function createCamera() {
     5000,                                     // far clipping plane
   );
 
-  // move the camera back so we can view the scene
-  // camera.position.set(0, 80, 260);
-  // camera.rotation.set(-Math.PI / 4, 0, 0);
-  
-  // camera.position.set( -156.66,  52.09, -119.37);
-  // camera.rotation.set(-1.48, -1.48, -1.48);
-
-  camera.position.set(200,0,0);
+  camera.position.set(200, 0, 0);
   camera.rotation.set(0, 0, 0);
 
   return camera;
