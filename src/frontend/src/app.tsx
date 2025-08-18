0196be2a29a82cd0245a1c3e3de91620610c625a
MatGew99 <mathias.gewissen@htw-dresden.de>
2025-08-18 16:47:47 +0200

    renaming

--- src/frontend/src/app.tsx
+++ src/frontend/src/app.tsx
@@ -1,5 +1,5 @@
-import { WidgetContainerSlider } from '@/containers/widget_container_slider';
-import { WidgetContainerTopbar } from '@/containers/widget_container_topbar';
+import { WidgetContainerSlider } from '@/containers/container_slider';
+import { WidgetContainerTopbar } from '@/containers/container_topbar';
 import { ThreeJSEngineProvider } from '@/context/context_three_js_engine';
 import ThreeJSScene from '@/threeJS/three_js_scene';
 
