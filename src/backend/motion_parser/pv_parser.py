5ce5ca191bd5097084738151ad4dfa4407d2f976
HTW Dresden <htwdresden@MacBook-Pro-von-HTW.local>
2026-01-29 09:26:19 +0100

    git pull fixing

--- src/backend/motion_parser/pv_parser.py
+++ src/backend/motion_parser/pv_parser.py
@@ -7,6 +7,7 @@ from pymotion.ops.skeleton import fk
 
 class PVParser:
     def __init__(self, file_path: str, descriptor_file: str):
+        pass
         # reader = MotionDataReader(file_path, descriptor_file)
         # if reader.positions is None:
         #     raise ValueError("reader.positions ist None – Datei wurde nicht korrekt geladen")
@@ -15,18 +16,19 @@ class PVParser:
         # self.joint_names = reader.generateNameList()
         # self.joint_graph = reader.joint_graph
 
-        workspacefolder = Path.cwd()
-        skeleton = {
-            "joint-graph": self.joint_graph
-        }
-        with open(Path.joinpath(workspacefolder, f"data/json/{Path(file_path).stem}.json"), "w") as f:
-            json.dump(skeleton, f, indent=2)
+        # workspacefolder = Path.cwd()
+        # skeleton = {
+        #     "joint-graph": self.joint_graph
+        # }
+        # with open(Path.joinpath(workspacefolder, f"data/json/{Path(file_path).stem}.json"), "w") as f:
+        #     json.dump(skeleton, f, indent=2)
 
     def save_npy(self, out_path: str):
-        arr = self.positions
-        if arr is None:
-            raise ValueError("self.positions ist None – es wurde keine Datenmatrix gesetzt.")
-        a = np.ascontiguousarray(arr)
-        np.save(out_path, a)
-        print(f"Saved global positions array with shape {arr.shape}")
-        print(f"Datei gespeichert: {out_path}")
+        pass
+        # arr = self.positions
+        # if arr is None:
+        #     raise ValueError("self.positions ist None – es wurde keine Datenmatrix gesetzt.")
+        # a = np.ascontiguousarray(arr)
+        # np.save(out_path, a)
+        # print(f"Saved global positions array with shape {arr.shape}")
+        # print(f"Datei gespeichert: {out_path}")
